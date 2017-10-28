4f30e402f83d9e26f6387e7967fa21401a672e0b
arthurandrade <arthurandrade186@gmail.com>
2017-10-28 12:32:24 -0200

    Add gelocation support via geolocation api

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -1,4 +1,40 @@
-const path = require('path')
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
-const publicPath = path.join(__dirname, '../public')
-console.log(publicPath);
+const { generateMessage, generateLocationMessage } = require('./utils/message');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
+  socket.on('createMessage', (message, callback) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('this is from the server');
+  });
+
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitute, coords.longitude));
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
